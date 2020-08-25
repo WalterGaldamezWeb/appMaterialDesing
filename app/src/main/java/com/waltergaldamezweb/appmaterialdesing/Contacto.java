968a614ca90ea35528e91cbea08c8453b679f2a6
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-08-25 11:13:56 -0600

    Activity Contacto Agregado

---
+++ app/src/main/java/com/waltergaldamezweb/appmaterialdesing/Contacto.java
@@ -0,0 +1,14 @@
+package com.waltergaldamezweb.appmaterialdesing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacto_activity);
+    }
+}
